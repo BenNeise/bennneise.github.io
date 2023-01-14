5a57279700627d5294d2eb16fb7d318c1b4a51ab
Ben Neise <ben@neise.co.uk>
2023-01-14 10:24:00 +0000

    Fix for permissions issue

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,11 +7,14 @@ if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
+git config --global user.email "ben@neise.co.uk"
+git config --global user.name "Ben Neise"
+git config --global --add safe.directory $(pwd)
+
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
 if [ -f Gemfile ]; then
+    sudo chown -R $(whoami):$(whoami) /usr/local/rvm/gems/default
     bundle install
 fi
 
-git config --global user.email "ben@neise.co.uk"
-git config --global user.name "Ben Neise"
