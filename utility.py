4dae599ccddf05ed7c8d5c0f000af5f7ab77494f
Ben Neise <ben@neise.co.uk>
2023-12-07 15:03:55 +0000

    Improved ability to find unused images

--- utility.py
+++ utility.py
@@ -3,18 +3,38 @@ from pathlib import Path
 
 # Find unused images
 
+extensions = {'.png', '.gif', '.jpg'}
+
+excluded_images = [
+    "android-chrome-192x192.png",
+    "android-chrome-512x512.png",
+    "apple-touch-icon.png",
+    "favicon-16x16.png",
+    "favicon-32x32.png"
+]
+
+
 image_links = set()
+# Get all of the HTML files from the _site folder
 for path in Path('./_site/').rglob('*.html'):
+    # Read the data from the file
     data = Path(path).read_text()
+    # Parse the HTML
     soup = BeautifulSoup(data, features="html.parser")
+    # Get all of the IMG tags
     for link in soup.find_all('img'):
+        # Get the IMG Source
         image_link = link.get('src')
+        # If it starts with assets
         if image_link.startswith("/assets/"):
+            print(f'Image link: {image_link}')
+            print(f'Exists: {Path("." + image_link).is_file()}') 
             image_links.add(image_link.replace("/assets/",""))
 
-extensions = {'.png', '.gif', '.jpg'}
+# Get all of the files in Assets
 for path in Path('./assets/').rglob('*.*'):
+    # Limit to image files
     if path.suffix in extensions:
-        filename = path.name.replace("assets/","")
-        if filename not in image_links:
-            print(f"Image {filename} is not used")
+        filename = str(path).replace("assets/","")
+        if filename not in image_links and filename not in excluded_images:
+            print(f"Image: {filename} is not used")
